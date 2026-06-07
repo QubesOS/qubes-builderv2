QubesOS/qubes-builderv2

commit 02b5c52bc92785a19c8359a9eb88e2b3d38ff3a5

Parse 'devel' version as '99.0'
This (hopefully) guarantee to be always newer than any released version.

QubesOS/qubes-issues#10478

qubesbuilder/config.py
@@ -627,12 +627,15 @@ class Config:
         return config_path
 
     def parse_qubes_release(self):
+        qubes_release = self.qubes_release
+        if qubes_release == "devel":
+            qubes_release = "99.0"
         parsed_release = QUBES_RELEASE_RE.match(
-            self.qubes_release
+            qubes_release
         ) or QUBES_RELEASE_RE.match(QUBES_RELEASE_DEFAULT)
         if not parsed_release:
             raise ConfigError(
-                f"Cannot parse Qubes OS release: '{self.qubes_release}'"
+                f"Cannot parse Qubes OS release: '{qubes_release}'"
             )
         return parsed_release
 

commit 1b2b55423df53014966b40539d3182149078c485

Fix template version for devel templates
Make the 99.0 actually pass validation.

Fixes: 02b5c52 "Parse 'devel' version as '99.0'"

qubesbuilder/config.py
@@ -45,7 +45,7 @@ from qubesbuilder.plugins import (
 )
 from qubesbuilder.template import QubesTemplate
 
-QUBES_RELEASE_RE = re.compile(r"r([1-9]\.[0-9]+).*")
+QUBES_RELEASE_RE = re.compile(r"r([1-9]+\.[0-9]+).*")
 QUBES_RELEASE_DEFAULT = "r4.2"
 
 
@@ -616,7 +616,7 @@ class Config:
     def parse_qubes_release(self):
         qubes_release = self.qubes_release
         if qubes_release == "devel":
-            qubes_release = "99.0"
+            qubes_release = "r99.0"
         parsed_release = QUBES_RELEASE_RE.match(
             qubes_release
         ) or QUBES_RELEASE_RE.match(QUBES_RELEASE_DEFAULT)